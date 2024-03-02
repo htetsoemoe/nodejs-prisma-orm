22b42a1b9d8f6738b33fdb28e77ccfcdbe7d5ec5
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-03-02 11:48:47 +0630

    insert data using prisma orm

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { PrismaClient } from "@prisma/client";
 
 const app = express()
+app.use(bodyParser.json()) // Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
+
+const prisma = new PrismaClient()
+
+app.post('/articles', async (req, res) => {
+    /*
+        await prisma.article.create({
+            data: req.body
+        })
+        res.json({
+            success: true,
+        })
+    */
+    await prisma.article.createMany({
+        data: req.body
+    })
+    res.json({ success: true })
+})
 
 app.get('/', (req, res) => res.send("Hello, World!"))
 
